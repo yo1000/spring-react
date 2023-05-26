35e8057162e8380d0e740e283a8801ba3abda4d7
YO!CHI KIKUCHI <i@yo1000.com>
2023-05-27 01:14:09 +0900

    Improve signed on user menu position

--- src/main/resources/frontend/src/components/Header.js
+++ src/main/resources/frontend/src/components/Header.js
@@ -200,7 +200,7 @@ export default function Header({primaryCategories}) {
                             leaveFrom="opacity-100 translate-y-0"
                             leaveTo="opacity-0 translate-y-1"
                           >
-                            <Popover.Panel className="absolute left-1/2 z-10 mt-2 flex w-screen max-w-min -translate-x-1/2 px-4">
+                            <Popover.Panel className="absolute -right-4 z-10 mt-2 flex w-screen max-w-min px-4">
                               <div className="w-44 shrink rounded-lg bg-white p-2 text-sm font-semibold leading-6 text-gray-900 shadow-md ring-1 ring-gray-900/5">
                                 <a className="block p-2 hover:text-indigo-600" onClick={() => {
                                   localStorage.setItem("idtoken", auth.user?.id_token)
